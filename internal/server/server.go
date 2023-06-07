fa26ac3d4b4a0d09aa1ff1f7eaeef89d6e0c8927
PRATHAM MORE <pratham02more20@gmail.com>
2023-06-07 10:06:16 +0530

    routes defined

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,6 +4,11 @@ import(
 	"flag"
 	"os"
 	"time"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/websocket/v2"
+	"github.com/gofiber/fiber/middleware/logger"	
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/template/html"
 )
 
 var (
@@ -19,10 +24,19 @@ func Run() error {
 		*addr = ":8080"
 	}
 
+	engine := html.New("./views",".html")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+	app.Use(cors.New())
+	app.Use(logger.New())
+
 	app.Get("/", handlers.Welcome)
 	app.Get("/room/create", handlers.RoomCreate)
 	app.Get("/room/:uuid",handlers.Room)
-	app.Get("/room/:uuid/websocket",)
+	app.Get("/room/:uuid/websocket", websocket.New(handlers.RoomWebsocket, websocket.Config{
+		HandshakeTimeout: 10 * time.Second,
+	}))
 	app.Get("/room/:uuid/chat",handlers.RoomChat)
 	app.Get("/room/:uuid/chat/websocket",websocket.New(handlers.RoomChatWebsocket))
 	app.Get("/room/:uuid/viewer/websocket",websocket.New(handlers.RoomViewerWebsocket))
